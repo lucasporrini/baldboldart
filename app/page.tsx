8596bf43b04a31aeb630acbddaa07995be0e90c7
Lucas <50617944+lucasporrini@users.noreply.github.com>
2025-02-10 15:48:25 +0100

    fix(core): fix useless import

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { Orchestrator } from "@/lib/components/orchestrator";
-import { useMobile } from "@/lib/hooks/use-mobile";
 
 export default function Home() {
-  const isMobile = useMobile();
-
   return (
     <main className="bg-background">
       <Orchestrator />
