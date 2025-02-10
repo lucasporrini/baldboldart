8ee996a1345e0f90d53981acd07cc1690a07e5e3
Lucas <50617944+lucasporrini@users.noreply.github.com>
2025-02-10 16:44:49 +0100

    feature(core): added popup

--- lib/components/hero/hero-mobile.tsx
+++ lib/components/hero/hero-mobile.tsx
@@ -44,8 +44,8 @@ export const HeroMobile = () => {
     <div className="relative w-full h-screen flex flex-col overflow-hidden">
       <AnimatedCubes />
 
-      <div className="absolute -top-1/4 left-1/2 -translate-x-1/2 w-[1000px] h-[calc(114vh)] bg-orange rounded-full"></div>
-      <div className="z-20 absolute bottom-[60px] flex items-center justify-center right-1/2 translate-x-1/2 size-[300px] bg-background rounded-full">
+      <div className="absolute -top-1/4 left-1/2 -translate-x-1/2 w-[1000px] h-[calc(113vh)] bg-orange rounded-full"></div>
+      <div className="z-20 absolute bottom-[60px] flex items-center justify-center right-1/2 translate-x-1/2 size-[30vh] bg-background rounded-full">
         <Image
           src="/hero-mobile.webp"
           alt="hero"
